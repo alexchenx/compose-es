6a57793f9b7d460664d81110b534309184a14ba0
chenxie <chenxie@nggame888.com>
2025-07-21 14:52:20 +0800

    update

--- scripts/clean_index.sh
+++ scripts/clean_index.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 # crontab -l
-# 0 3 * * * bash /data/compose/clean_index.sh >> /var/log/clean_index.log 2>&1
+# 10 8 * * * bash /data/compose/clean_index.sh >> /var/log/clean_index.log 2>&1
 
 echo "##################################### $(date "+%Y-%m-%d %H:%M:%S %z") #####################################"
 
 ES_HOST="http://localhost:9200"
-RETAIN_DAYS=7
+RETAIN_DAYS=1
 
 INDEX_PREFIXES=(
   "test-applications-"
@@ -30,14 +30,17 @@ for PREFIX in "${INDEX_PREFIXES[@]}"; do
   echo ""
   echo "正在处理前缀：$PREFIX"
 
-  INDICES=$(curl "${CURL_OPTS[@]}" "$ES_HOST/_cat/indices/${PREFIX}*?h=index" | sort)
+  INDICES_INFO=$(curl "${CURL_OPTS[@]}" "$ES_HOST/_cat/indices/${PREFIX}*?h=index,store.size" | sort)
 
-  if [[ -z "$INDICES" ]]; then
+  if [[ -z "$INDICES_INFO" ]]; then
     echo "没有匹配前缀 $PREFIX 的索引"
     continue
   fi
 
-  for index in $INDICES; do
+  while read -r line; do
+    index=$(echo "$line" | awk '{print $1}')
+    size=$(echo "$line" | awk '{print $2}')
+
     DATE_PART=$(echo "$index" | grep -oE '[0-9]{4}\.[0-9]{2}\.[0-9]{2}')
     if [[ -z "$DATE_PART" ]]; then
       echo "跳过非日期型索引: $index"
@@ -45,13 +48,13 @@ for PREFIX in "${INDEX_PREFIXES[@]}"; do
     fi
 
     if [[ "$DATE_PART" < "$DATE_LIMIT" ]]; then
-      echo -n "删除索引: $index "
+      echo -n "删除索引: $index (大小: $size) "
       curl "${CURL_OPTS[@]}" -X DELETE "$ES_HOST/$index"
       echo
     else
-      echo "保留索引: $index"
+      echo "保留索引: $index (大小: $size)"
     fi
-  done
+  done <<< "$INDICES_INFO"
 done
 
 echo -e "\nDone.\n"
